00f7425e4e756cb284d1112a7f2d730885bce102
rbulha <rbulha@gmail.com>
2010-03-29 19:57:47 +0000

    Function Capture is prepared to: TSW1205 send data in 12bits masked in 16bits Big-endian.

--- TSW1205.py
+++ TSW1205.py
@@ -6,14 +6,43 @@ class CSerialCaptureBase():
     def __init__(self):
         print '[CSerialCaptureBase] - Init'
     def Connect(self, iport):
-        self.ser = serial.Serial(iport)
-        self.ser.timeout = 10
+        try:
+            self.ser = serial.Serial(port=iport,baudrate=460800)
+            self.ser.timeout = 10
+        except serial.SerialException:
+            print '[CSerialCaptureBase.Connect] - Connect fail'
+    def InitTSW1205(self):
+        try:
+            que1 = struct.pack('BB',3,16)
+            self.ser.write(que1)
+            time.sleep(0.1)        
+            que2 = struct.pack('BB',3,17)
+            self.ser.write(que2)
+            time.sleep(0.1)            
+            cmd = struct.pack('38B',0x00,0xFF,0x00,0x00,0x12,0x20,0x02,0x08,0x13,0x00,0x0F,0x01,0x05,0x80,0x05,0x00
+            ,0x05 ,0x40 ,0x05 ,0x00 ,0x0F ,0x00 ,0x0D ,0xFF ,0x0E ,0xFF ,0x05 ,0x80 ,0x05 ,0x00 ,0x05 ,0x40
+            ,0x05 ,0x00 ,0x0D ,0x00 ,0x0E ,0x00 )
+            self.ser.write(cmd)
+            time.sleep(0.1)  
+            self.ser.flushInput()          
+        except serial.SerialException:
+            print '[CSerialCaptureBase.InitTSW1205] - Init fail'
     def Disconnect(self):
         self.ser.close()
+    def SeletcDataLength(self,data_length):
+        try:
+            #if data_length == 4096:
+            cmd = struct.pack('BB',0x4,0x12)
+            self.ser.write(cmd)            
+        except serial.SerialException:
+            print '[CSerialCaptureBase.SeletcDataLength] - SeletcDataLength fail'
     def SelectChannel(self,ch):
-        if ch == 1:
-            cmd = struct.pack('BBBB',2,8,19,0)
-            self.ser.write(cmd)
+        try:
+            if ch == 1:
+                cmd = struct.pack('BBBB',2,8,19,0)
+                self.ser.write(cmd)
+        except serial.SerialException:
+            print '[CSerialCaptureBase.SelectChannel] - SelectChannel fail'
     def OneShotCapture(self, iport, ch, Npoints):
         self.Connect(iport)
         self.SelectChannel(ch)
@@ -22,16 +51,20 @@ class CSerialCaptureBase():
         return data        
     def Capture(self,Npoints):
         cmd = struct.pack('6B',0,255,0,0,1,3)
-        self.ser.write(cmd)
-        time.sleep(0.1) 
-        unpack_format = '%dH'%Npoints
-        retorno = self.ser.read(Npoints)
         self.ser.flushInput()
-        if len(retorno) == Npoints:
-            dados = struct.unpack(unpack_format,retorno)
-            return dados
+        self.ser.write(cmd)
+        time.sleep(0.01) 
+        # TSW1205 send data in 12bits masked in 16bits Big-endian
+        unpack_format = '>%dH'%(Npoints)
+        retorno = self.ser.read(Npoints*2)
+        if len(retorno) == (Npoints*2):
+            dados_unp = struct.unpack(unpack_format,retorno)
+            dados_m = []
+            for valor in dados_unp:
+                dados_m.append((valor&0x0FFF)) #take only the 12bts
+            return dados_m
         else:
-            print 'Falha na captura'
+            print '[CSerialCaptureBase.Capture] Capture fail'
             return []
     def FindTSW1205(self):
         available = []
@@ -60,9 +93,23 @@ class CSerialCaptureBase():
 
 def tester():
     ser = CSerialCaptureBase()
-    ser.Connect(6)      
+    ser.Connect(5)
+    ser.InitTSW1205()    
+    time.sleep(0.1)  
     ser.SelectChannel(1)
-    print ser.Capture(1000)
+    time.sleep(0.1)
+    ser.SeletcDataLength(4096)
+    time.sleep(0.1)
+    t0 = time.time()
+    dados = ser.Capture(4096)
+    t1 = time.time()
+    ser.Disconnect()
+    f = open('teste.txt','w')
+    for valor in dados:
+        saida = '%d\n'%(valor)
+        f.write(saida)
+    f.close()
+    print 'Captura demorou: %f sec'%(t1-t0)
     
 if __name__ == '__main__':
     tester()    
